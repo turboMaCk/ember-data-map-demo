887393d0aaa534e06918b40ae13ca5472e5a344b
Marek Fajkus <marek.faj@gmail.com>
2014-02-10 23:08:23 +0100

    Licence first

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,8 +1,15 @@
+/**
+ * @description Demo app using Google Map Api 3 in Ember.js and Ember Data base single page app.
+ * @author Marek Fajkus @turbo_MaCk
+ * @license http://sam.zoy.org/wtfpl WTFPL v.2
+ * @version 0.0.1
+ */
+
 var EmberDataMapDemo = window.EmberDataMapDemo = Ember.Application.create();
 
 /* Order and include as you please. */
-require('scripts/controllers/*');
 require('scripts/store');
+require('scripts/controllers/*');
 require('scripts/models/*');
 require('scripts/routes/*');
 require('scripts/views/*');
